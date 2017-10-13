3760988a17d79c9a71f9b40a2faf8a48f9713176
daves-devel <d.chakkuthara@gmail.com>
2017-10-12 20:49:21 -0400

    adding timing

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -8,6 +8,8 @@
 #include <algorithm>
 #include <map>
 #include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
 
 
 #include "graphics.h"
@@ -48,6 +50,18 @@ enum SWITCH_BLOCK_MODE{
 #ifndef SWBMODE
 #define SWBMODE CROSSBAR
 #endif
+
+double get_wall_time(){
+    struct timeval time;
+    if (gettimeofday(&time,NULL)){
+        //  Handle error
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+double get_cpu_time(){
+    return (double)clock() / CLOCKS_PER_SEC;
+}
 
 class Segment{
 private:
@@ -1224,6 +1238,10 @@ int main(int argc , char ** argv){
     pthread_t threads[NUM_THREADS];
     int rc, thread_index;
 
+    //  Start Timers
+    double wall0 = get_wall_time();
+    double cpu0  = get_cpu_time();
+
 	if (argc < 2) {
 		cerr << "Error: Missing filename! Use " << argv[0] << " <filename>" << std::endl;
 		return -1;
@@ -1274,9 +1292,16 @@ int main(int argc , char ** argv){
 		update_message(message.c_str());
 	}
 	else	update_message("Done!");
+    
+    //  Stop timers
+    double wall1 = get_wall_time();
+    double cpu1  = get_cpu_time();
+
 	cout << "Using switch block mode " << ((SWBMODE==CROSSBAR) ? "Crossbar" : "Wilton") << endl;
 	cout << "Used " << utilvars::routing->routingSegmentsUsed() << " wire segments." << endl;
 	cout << "Widest channel used: " << utilvars::routing->maxW() << endl;
+    cout << "Wall Time = " << wall1 - wall0 << endl;
+    cout << "CPU Time  = " << cpu1  - cpu0  << endl;
 	event_loop(NULL, NULL, NULL, drawscreen);
 	return 0;
 }
